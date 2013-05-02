7923ea7e11c1ebbfcdbf7c9fef48649ed92d3e8b
stefo <stefo@3141-slave.(none)>
2013-05-02 16:38:51 +0200

    Inseriti commenti alla classe PeerTable
    Commentata la classe PeerTable ed effettuata qualche innoqua
    modifica per rendere il codice un pochino piu' guardabile.

--- PeerTable.java
+++ PeerTable.java
@@ -5,17 +5,26 @@ public class PeerTable {
 
 	private Vector<PeerTableData> data;
 	
-	public PeerTable() {}
+	public PeerTable() {
+		data = new Vector<PeerTableData>();
+	}
 	
 	public PeerTable(PeerTableData d) {
 		this.data.add(d);
 	}
 	
+	/*
+	 * Aggiunge un elemento di tipo PeerTableData alla PeerTable
+	 * */
 	public void add(PeerTableData pd) {
 		
 		this.data.add(pd);
 	}
 	
+	/*
+	 * Ritorna un elemento di tipo PeerTableData contenente i dati relativi al coordinatore
+	 * per la risorsa a cui la PeerTable si riferisce.
+	 * */
 	public PeerTableData getCoord() {
 		
 		for(int i=0;i<data.size();++i) {
@@ -26,6 +35,10 @@ public class PeerTable {
 		return null;
 	}
 	
+	/*
+	 * Ritorna un elemento di tipo PeerTableData contenente tutte le informazioni su un peer
+	 * avente come indirizzo ip quello passato come parametro se esiste, null altrimenti
+	 * */
 	public PeerTableData getIP(String ip) {
 		for (int i=0 ; i < data.size() ; ++i) {
 			if(data.get(i).peer == ip)
@@ -34,18 +47,30 @@ public class PeerTable {
 		return null;
 	}
 	
+	/*
+	 * Elimina un elemento dalla PeerTable
+	 * */
 	public void remove(PeerTableData ptd) {
 		data.remove(ptd);
 	}
 	
+	/*
+	 * Getter per la PeerTable
+	 * */
 	public Vector<PeerTableData> get() {
 		return this.data;
 	}
 	
+	/*
+	 * Setter per la PeerTable
+	 * */
 	public void set(Vector<PeerTableData> d) {
 		this.data = d;
 	}
 	
+	/*
+	 * Ritorna la distanza media dei peer nella PeerTable
+	 * */
 	public float getAvgDist() {
 		float avg = 0;
 		int i;
@@ -55,10 +80,15 @@ public class PeerTable {
 		return avg/i;
 	}
 	
+	/*
+	 * Ritorna l'indirizzo ip del peer con distanza piu' bassa.
+	 * 
+	 * Utilizzato prima del trasferimento di una risorsa per decidere a chi richiederla.
+	 * */
 	public String getMinDistPeer() {
-		float min = 999;
-		int pos = -1;
-		for(int i=0;i<this.data.size();++i) {
+		float min = this.data.get(0).dist;
+		int pos = 0;
+		for(int i=1;i<this.data.size();++i) {
 			if(this.data.get(i).dist<min) {
 				min = this.data.get(i).dist;
 				pos = i;
