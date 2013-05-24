58d32e6bbc613bcb2ff9170833ecfe6496fc1892
Daniele <daniele@daniele-Aspire-7750G.(none)>
2013-05-24 17:49:47 +0200

    aggiunti commenti in stile javadoc della classe PeerTableData

--- PeerTableData.java
+++ PeerTableData.java
@@ -4,11 +4,21 @@ import java.io.Serializable;
 
 public class PeerTableData implements Serializable {
 
+	private static final long serialVersionUID = 1L;
+	
 	public String peer;
 	public float dist;
 	public boolean down;
 	public boolean coordinator;
 	
+	/**
+	 * Costruttore della classe PeerTableData
+	 * 
+	 * @param peer indirizzo ip del peer
+	 * @param dist distanza dal peer
+	 * @param down flag che indica se il peer è down
+	 * @param coord flag che indica se peer è il coordinatore
+	 */
 	public PeerTableData(String peer, float dist, boolean down, boolean coord) {
 		this.peer = peer;
 		this.dist = dist;
